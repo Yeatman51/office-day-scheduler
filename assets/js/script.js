2afa3a30557d0e925b7b028d4d5214bc7812d4d0
Jon Yeatman <jonlyeatman@gmail.com>
2021-02-25 22:21:22 -0500

    got the time and date to Display on the screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
-$( function() {
-    $( "#sortable" ).sortable();
-    $( "#sortable" ).disableSelection();
-  } );
+let dateTime =  moment().format('llll'); 
+$("#date-time").text(dateTime);
+console.log(dateTime);
+
+var gradDate = moment("2021-06-15").format("MMM Do, YYYY");
+$("#1a").text(gradDate);
