f41de2fa5f8195b261ff7199cee6dbd59b586399
Jon Yeatman <jonlyeatman@gmail.com>
2021-02-27 21:25:39 -0500

    updated read me fixed time updating br

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # office-day-scheduler
+
+This is a office day scheduler
+Can use this to help keep track of your meetings throughout the workday
+The time will display automatically and constantly be up-to-date
+You can type into each time section and save your meetings in each hourly category
+
+https://yeatman51.github.io/office-day-scheduler/
+
+https://github.com/Yeatman51/office-day-scheduler
+
+![screen-shot-scheduler](assets/img/screen-shot-scheduler.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,38 @@
-let dateTime =  moment().format('llll'); 
-$("#date-time").text(dateTime);
-console.log(dateTime);
 
-var gradDate = moment("2021-06-15").format("MMM Do, YYYY");
-$("#1a").text(gradDate);
+// Date and time function updating every second to keep time up-to-date
+function updateTime () {
+    let dateTime =  moment().format('llll'); 
+    $("#date-time").text(dateTime);
+    console.log(dateTime);
+}
+
+setInterval(updateTime, 1000);
+updateTime ();
+
+// Local storage saving text
+let storageInput = document.querySelector('.storage');
+// let text = document.querySelector('.text');
+let button = document.querySelector('.saveBtn');
+
+storageInput.addEventListener('input', letter => {
+    console.log(letter.target.value)
+    console.log(storageInput.textContent)
+    storageInput.textContent = letter.target.value
+})
+
+let saveToLocalStorage = () => {
+    localStorage.setItem('textInput', storageInput.textContent)
+}
+
+button.addEventListener('click', saveToLocalStorage)
+
+
+
+// var gradDate = moment("2021-06-15").format("MMM Do, YYYY");
+// $("#1a").text(gradDate);
+
+// setInterval(() =>{
+//     let dateTime =  moment().format('llll'); 
+//     $("#date-time").text(dateTime);
+//     console.log(dateTime);
+// }, 1000);
